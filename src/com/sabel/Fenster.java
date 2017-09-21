d790435b0b1f45058edea91b941f0b958016930d
Nadine Richter <dine.richter@googlemail.com>
2017-09-21 09:05:47 +0200

    Project cleared from events

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -1,11 +1,8 @@
 package com.sabel;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class Fenster extends JFrame implements ActionListener{
+public class Fenster extends JFrame{
 
     private JPanel jPanel;
     private JButton jbtnRot;
@@ -17,19 +14,11 @@ public class Fenster extends JFrame implements ActionListener{
         super("Farbenspiel");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.initComponents();
-        this.initEvents();
         this.setSize(300, 300);
         this.setVisible(true);
 
     }
 
-    private void initEvents() {
-
-        jbtnRot.addActionListener(this);
-        jbtnBlau.addActionListener(this);
-        jbtnGelb.addActionListener(this);
-
-    }
 
     private void initComponents() {
         jPanel = new JPanel();
@@ -42,19 +31,4 @@ public class Fenster extends JFrame implements ActionListener{
         this.add(jPanel);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-      //        System.out.println(command + " wurde geklickt!");
-        switch(e.getActionCommand()){
-            case "Rot":
-                jPanel.setBackground(Color.RED);
-                break;
-            case "Gelb":
-                jPanel.setBackground(Color.YELLOW);
-                break;
-            case "Blau":
-                jPanel.setBackground(Color.BLUE);
-                break;
-        }
-    }
 }
